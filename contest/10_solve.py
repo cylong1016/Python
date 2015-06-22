564eaf177d28f93c80aaf438209e4404a47a7b1b
陈云龙 <cyl19941016@sina.com>
2015-06-22 22:12:50 +0800

    完成第10题，终于全部搞定！

--- contest/10_solve.py
+++ contest/10_solve.py
@@ -15,6 +15,44 @@
 [a,b,r2]
 """
 
+import urllib
+import json
+
 class Solution:
-    def solve(self):
-        pass
+	def solve(self):
+		url = "http://112.124.1.3:8060/getData/101.json"
+		page = urllib.urlopen(url)
+		html = page.read()
+		
+		data = json.loads(html)['data']
+		n = len(data)
+		sumX = 0.0
+		sumY = 0.0
+		lxy = 0.0
+		lxx = 0.0
+		for r in data:
+			agepreg = r[6]
+			totalwgt_oz = r[4]
+			sumX += agepreg
+			sumY += totalwgt_oz
+			lxy += totalwgt_oz * agepreg
+			lxx += agepreg ** 2
+		meanX = sumX / n
+		meanY = sumY / n
+		lxy -= sumX * sumY / n
+		lxx -= sumX ** 2 / n
+		b = lxy / lxx
+		a = meanY - b * meanX
+		
+		
+		SR = 0.0
+		ST = 0.0
+		for r in data:
+			SR += (b * r[6] + a - meanY) ** 2
+			ST += (r[4] - meanY) ** 2
+		r2 = SR / ST
+		
+		return [a, b, r2]
+
+s = Solution()
+print s.solve()
